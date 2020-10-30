f13fbae6bc5837c2e24546f51bc7762523531146
Suzanne Cabral <suzannecabral@live.com>
2020-10-30 09:10:08 -0700

    Get all works for projects and actions

--- actionsRouter.js
+++ actionsRouter.js
@@ -7,7 +7,14 @@ const router = express.Router();
 
 //endpoints
 router.get('/', (req, res) => {
-    res.status(200).json({message:`Actions router is working`});
+    Actions.get()
+    .then(data=>{
+        res.status(200).json(data)
+    })
+    .catch(err=>{
+        console.log(err);
+        res.status(500).json({message:`server error retrieving actions data`});
+    })
 });
 
 //middleware
